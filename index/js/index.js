281349b366ae8faa5e2232edfa3c4e067aac8e60
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2017-04-29 20:04:23 +0300

    add animation button

--- index/js/index.js
+++ index/js/index.js
@@ -11,7 +11,7 @@ function onLoad()
         fullscreenButton:false,
         baseLayerPicker:false,
         sceneModePicker:false,
-        animation : false,
+        //animation : false,
         homeButton:false,
         geocoder:false,
         scene3DOnly:true,
@@ -36,8 +36,9 @@ function onLoad()
     });
     viewer.imageryLayers.addImageryProvider(tms);
     viewer.extend(Cesium.viewerDragDropMixin);
-    viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample1.czml'));
-    viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample1_1.czml'));
+    //viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample1.czml'));
+    //viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample1_1.czml'));
+    viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/json.czml'));
     //Show a pop-up alert if we encounter an error when processing a dropped file
     viewer.dropError.addEventListener(function(dropHandler, name, error) {
         console.log(error);
