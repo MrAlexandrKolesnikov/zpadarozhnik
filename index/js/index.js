977478f961484972b3a28705bba28afedb3b9745
KolesnikovAlexsandr <mister.sasha.kolesnikov@yandex.ru>
2017-04-30 13:13:29 +0300

    commit index.js

--- index/js/index.js
+++ index/js/index.js
@@ -38,45 +38,19 @@ function onLoad()
     });
     viewer.imageryLayers.addImageryProvider(tms);
     viewer.extend(Cesium.viewerDragDropMixin);
-    //viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample1.czml'));
-    //viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample1_1.czml'));
 
-    viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/sample8.czml'));
-    //viewer.dataSources.add(Cesium.CzmlDataSource.load('/Cesium/json.czml'));
-
-
+    viewer.dataSources.add(Cesium.CzmlDataSource.load('/Data/landsat_8.czml'));
+    viewer.dataSources.add(Cesium.CzmlDataSource.load('/Data/NOAA-18.czml'));
     //Show a pop-up alert if we encounter an error when processing a dropped file
     viewer.dropError.addEventListener(function(dropHandler, name, error) {
         console.log(error);
         window.alert(error);
     });
 
-    // var pinBuilder = new Cesium.PinBuilder();
-    //
-    // var bluePin = viewer.entities.add({
-    //     description: '<iframe width="400" height="300" src="https://www.youtube.com/embed/ACJJi5re5NU" frameborder="0" allowfullscreen></iframe>',
-    //     position : Cesium.Cartesian3.fromDegrees(-75.170726, 39.9208667),
-    //     billboard : {
-    //         image : pinBuilder.fromColor(Cesium.Color.ROYALBLUE, 48).toDataURL(),
-    //         verticalOrigin : Cesium.VerticalOrigin.BOTTOM
-    //     }
-    // });
-    // cos
-    //viewer.zoomTo(bluePin);
-
     viewer.scene.sun.destroy();
     viewer.scene.moon.destroy();
     viewer.infoBox.frame.removeAttribute('sandbox');
-    viewer.scene.sun.destroy();
-    viewer.scene.moon.destroy();
-    var data = Cesium.CzmlDataSource.load('/Cesium/json5.czml');
-    viewer.dataSources.add(data);
-
 
 }
 
-
-/*var checkTime = setInterval(function() {
-    console.log(viewer.clock.currentTime);
-}, 1000);*/
 
